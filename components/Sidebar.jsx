c0956e3120538dee4f107f1cd567f3c9dfc1af83
OzanOcak <ozanocak@gmail.com>
2022-07-18 16:37:05 -0400

    side nav - mini profile

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -16,7 +16,7 @@ function Sidebar() {
   return (
     <div className="hidden sm:flex flex-col p-2  fixed h-full">
       {/** Twitter Logo */}
-      <div className="w-[3rem] hoverEffect hover:bg-blue-100">
+      <div className="w-[3rem] hoverEffect hover:bg-blue-100 my-5 ">
         <Image
           width="50"
           height="50"
@@ -44,6 +44,21 @@ function Sidebar() {
       >
         Tweet
       </button>
+
+      {/** mini - profile */}
+      <div className="hoverEffect flex justify-center items-center xl:justify-start mt-auto my-5">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/en/a/a5/Donald_Duck_angry_transparent_background.png"
+          width={70}
+          height={70}
+          alt="d.duck"
+          className="rounded-full"
+        />
+        <div className="leading-5 hidden xl:inline text-gray-700">
+          <div className="font-bold">Donald Duck</div>
+          <div className="taxr-gray-500">@donaldduck</div>
+        </div>
+      </div>
     </div>
   );
 }
