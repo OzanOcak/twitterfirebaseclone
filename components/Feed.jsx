ef60d3da60d78d8fff02b189df32d9afb4e41bb3
OzanOcak <ozanocak@gmail.com>
2022-07-18 19:05:10 -0400

    feed & side nav fixing

---
+++ components/Feed.jsx
@@ -0,0 +1,17 @@
+import { SparklesIcon } from "@heroicons/react/outline";
+function Feed() {
+  return (
+    <div className="xl:ml-[23rem] sm:border-l sm:border-r border-gray-200xl:min-w-[36rem] sm:ml-[5rem]  flex-grow max-w-xl">
+      <div className="flex justify-between py-0 px-3 top-0 sticky z-10 bg-white border-b border-gray-200">
+        <h2 className="text-lg sm:text-xl py-[.6rem] font-bold cursor-pointer">
+          Home
+        </h2>
+        <div className="hoverEffect flex items-center justify-center hover:bg-gray-200">
+          <SparklesIcon className="h-[1.5rem] sm:h-[2rem]" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
--- components/SidebarMenuItem.jsx
+++ components/SidebarMenuItem.jsx
@@ -1,6 +1,6 @@
 function SidebarMenuItems({ Icon, text, active }) {
   return (
-    <div className="flex justify-start items-center hoverEffect h-[2.3rem] w-[9.8rem] text-gray-700 p-1 mb-3.5 xl:text-lg space-x-2 xl:mb-4 ">
+    <div className="flex justify-start items-center hoverEffect h-[2.3rem] w-[2.3rem] xl:w-[9.8rem] text-gray-700 p-1 mb-3.5 xl:text-lg space-x-2 xl:mb-4 ">
       <Icon className="h-[1.7rem] mr-1" />
       <div className={`${active && "font-bold"} hidden xl:inline`}>{text}</div>
     </div>
