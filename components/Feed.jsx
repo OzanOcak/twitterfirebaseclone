8f1121e22a67139ffcf33b477be521a369bb2b99
OzanOcak <ozanocak@gmail.com>
2022-07-19 12:02:05 -0400

    post feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,29 @@
 import { SparklesIcon } from "@heroicons/react/outline";
 import FeedInput from "./FeedInput";
+import FeedPost from "./FeedPost";
 function Feed() {
+  const posts = [
+    {
+      id: "1",
+      name: "Donald Duck",
+      username: "donaldduck",
+      userImg:
+        "https://upload.wikimedia.org/wikipedia/en/a/a5/Donald_Duck_angry_transparent_background.png",
+      img: "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80",
+      text: "nice view!",
+      timestamp: "2 hours ago",
+    },
+    {
+      id: "2",
+      name: "Donald Duck",
+      username: "donaldduck",
+      userImg:
+        "https://upload.wikimedia.org/wikipedia/en/a/a5/Donald_Duck_angry_transparent_background.png",
+      img: "https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80",
+      text: "wow!",
+      timestamp: "2 days ago",
+    },
+  ];
   return (
     <div className="xl:ml-[23rem] sm:border-l sm:border-r border-gray-200xl:min-w-[36rem] sm:ml-[5rem]  flex-grow max-w-xl">
       <div className="flex justify-between py-0 px-3 top-0 sticky z-10 bg-white border-b border-gray-200">
@@ -12,6 +35,9 @@ function Feed() {
         </div>
       </div>
       <FeedInput />
+      {posts.map((post) => (
+        <FeedPost key={post.id} post={post} />
+      ))}
     </div>
   );
 }
---
+++ components/FeedPost.jsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import {
+  ChartBarIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  TrashIcon,
+} from "@heroicons/react/outline";
+
+function FeedPost({ post }) {
+  return (
+    <div className="flex mx-auto w-full">
+      {/** user image */}
+      <div>
+        <Image
+          alt={post.name}
+          src={post.userImg}
+          width={50}
+          height={50}
+          className="rounded-full mr-3 mt-auto"
+        />
+      </div>
+
+      {/* right side */}
+      <div className="">
+        {/* Header */}
+
+        <div className="flex  justify-between">
+          {/* post user info */}
+          <div className="flex items-center space-x-1 whitespace-nowrap">
+            <span className="text-sm sm:text-[15px]">@{post.username} - </span>
+            <span className="text-sm sm:text-[15px] hover:underline">
+              {post.timestamp}
+            </span>
+          </div>
+
+          {/* dot icon */}
+          <DotsHorizontalIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2" />
+        </div>
+
+        {/* post text */}
+
+        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2">
+          {post.text}
+        </p>
+
+        {/* post image */}
+        <div className="rounded-2xl mr-2">
+          <Image alt={post.name} src={post.img} width={500} height={300} />
+        </div>
+
+        <div className="flex justify-between text-gray-500 p-2">
+          <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+          <HeartIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+          <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default FeedPost;
